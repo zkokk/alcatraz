b7436eaa670865238c2b8ac11ce68a54feb3c437
Ruslan Gladov <ruslangladov@Ruslans-MacBook-Pro.local>
2025-09-28 15:58:50 +0300

    Adding app and dockerfile

---
+++ web-app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+import socket
+
+app = Flask(__name__)
+
+@app.route("/api/ping", methods=["GET"])
+def ping():
+    hostname = socket.gethostname()
+    return jsonify({
+        "hostname": hostname,
+        "message": "pong"
+    })
+
+if __name__ == "__main__":
+    # Runs on all network interfaces, port 8080 by default
+    app.run(host="0.0.0.0", port=8080)
